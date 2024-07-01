bc33728456cf6fbc628405f36435b763f4412137
Stijn Denruyter <stijn@denruyter.net>
2024-07-01 17:34:01 +0200

    Update PSOSDT.ps1

--- Functions/PSOSDT.ps1
+++ Functions/PSOSDT.ps1
@@ -1,3 +1,17 @@
+Function Write-PSOSDTLogHeader
+{
+	[CmdletBinding()]
+	Param (
+		[Parameter(Mandatory = $True)]
+		[ValidateNotNullOrEmpty()]
+		[String]$Message
+	)
+	$DateConsole = Get-Date -Format "dd-MM-yyyy"
+	$TimeConsole = Get-Date -Format "HH:mm:ss"
+	$Message = "`r`n" + ('=' * 80) + "`r`n" + $DateConsole + " " + $TimeConsole + " " + $Message + "`r`n" + ('=' * 80)
+	Write-Host $Message -ForegroundColor Cyan
+}
+
 Function Invoke-PSOSDTProcess
 {
 	[CmdletBinding()]
@@ -26,20 +40,6 @@ Function Invoke-PSOSDTProcess
 	Else {
 		Write-Host "`r`nExit code: $($ExitCode)`r`n" -ForegroundColor Red
 	}
-}
-
-Function Write-PSOSDTLogHeader
-{
-	[CmdletBinding()]
-	Param (
-		[Parameter(Mandatory = $True)]
-		[ValidateNotNullOrEmpty()]
-		[String]$Message
-	)
-	$DateConsole = Get-Date -Format "dd-MM-yyyy"
-	$TimeConsole = Get-Date -Format "HH:mm:ss"
-	$Message = "`r`n" + ('=' * 80) + "`r`n" + $DateConsole + " " + $TimeConsole + " " + $Message + "`r`n" + ('=' * 80)
-	Write-Host $Message -ForegroundColor Cyan
 }
 
 Function Set-PSOSDTResizeOutputWindow
